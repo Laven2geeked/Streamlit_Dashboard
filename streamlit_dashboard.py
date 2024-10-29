fbcdb53dbd87941a490ab7c62c99069b0ecf2c3a
Laven2geeked <lavenctrl@gmail.com>
2024-10-29 11:49:00 +0300

    Initial commit with project setup

---
+++ streamlit_dashboard.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd 
+import plotly.express as px 
+
+@st.cache
+def load_data():
+    return pd.read_csv('dataset(1).csv')
+
+data = load_data()
+
+st.title("Streamlit Dashboard with Plotly")
+st.write("This is a simple example dashboard with plotly visualizations.")
+
+fig = px.line(data, x="year", y="population", color="sex", title="Population Over Time")
+st.plotly_chart(fig)
